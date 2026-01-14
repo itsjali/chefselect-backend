05ed0b94e50ffa3c1735d48449b12150f2b7fec1
Josh Ali <ali.josh@outlook.com>
2026-01-14 18:44:16 +0000

    add auth headers fixture and update test views

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
+import os
 import pytest
+import requests
 
 from app import create_app
 from app.models import db
 from app.config import TestConfig
+
+
+DEV_USER_EMAIL = os.getenv("DEV_USER_EMAIL")
+DEV_USER_PASSWORD = os.getenv("DEV_USER_PASSWORD")
+FLASK_URL = os.getenv("FLASK_URL")
 
 
 @pytest.fixture(scope="module")
@@ -24,6 +31,24 @@ def init_database():
         db.create_all()
         yield db
         db.drop_all()
+
+
+@pytest.fixture(scope="session")
+def access_token():
+    request = requests.post(
+        f"{FLASK_URL}/login",
+        json={"email": DEV_USER_EMAIL, "password": DEV_USER_PASSWORD},
+    )
+    assert request.status_code == 200
+    return request.json()["access_token"]
+
+
+@pytest.fixture
+def auth_headers(access_token):
+    return {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}"
+    }
 
 
 @pytest.fixture
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,36 +1,29 @@
 import requests
 import os
 
-from datetime import datetime
-from unittest import mock
+FLASK_URL = os.getenv("FLASK_URL")
 
 
 ####### CREATE RECIPE #######
 
-def test_create_recipe_view_success(valid_recipe_data):
-    app_url = os.getenv("FLASK_URL")
-    url = f"{app_url}/create-recipe"
-    
-    response = requests.post(url, json=valid_recipe_data)
 
+def test_create_recipe_view_success(auth_headers, valid_recipe_data):
+    url = f"{FLASK_URL}/create-recipe"    
+    response = requests.post(url, json=valid_recipe_data, headers=auth_headers)
     assert response.status_code == 200
 
-
-def test_create_recipe_view_failure_with_invalid_payload():
+def test_create_recipe_view_failure_with_invalid_payload(auth_headers):
     payload = {
         "description": 12,
         "ingredients": "fake ingredients",
         "instructions": {"Step 1": "Step 2"},
     }
     
-    app_url = os.getenv("FLASK_URL")
-    url = f"{app_url}/create-recipe"
-    
-    response = requests.post(url, json=payload)
-
+    url = f"{FLASK_URL}/create-recipe"
+    response = requests.post(url, json=payload, headers=auth_headers)
     response_data = response.json()
+    
     error_msg = response_data["errors"]
-
     assert response.status_code == 400
     assert "Not a valid string." in error_msg["description"] 
     assert "Not a valid list." in error_msg["ingredients"]
@@ -41,10 +34,7 @@ def test_create_recipe_view_failure_with_invalid_payload():
 ####### GET_RECIPES #######
 
 
-def test_get_recipes_view_success():
-    app_url = os.getenv("FLASK_URL")
-    url = f"{app_url}/get-recipes"
-
-    response = requests.post(url)
-
+def test_get_recipes_view_success(auth_headers):
+    url = f"{FLASK_URL}/get-recipes"
+    response = requests.get(url, headers=auth_headers)
     assert response.status_code == 200
